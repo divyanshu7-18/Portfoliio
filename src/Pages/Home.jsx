ef2afdaa685a69a291f2be77915d86f73931fd8e
Divyanshu Kumar <divyanshu07aug@gmail.com>
2026-02-23 21:43:35 +0530

    Added background and Navbar with the heroSection

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,7 @@
+import { Navbar } from "../components/Navbar";
 import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "../components/StarBackground";
+import { HeroSection } from "../components/HeroSection";
 
 export const Home = () => {
   return (
@@ -6,11 +9,14 @@ export const Home = () => {
     { /*theme toggle*/ }
     <ThemeToggle />
     { /*Background*/ }
-
+    <StarBackground />
     { /*Navbar*/ }
-
+    <Navbar />
     {/*Main Content*/}
+    <main>
+      <HeroSection />
 
+    </main>
     {/*Footer*/ }
   </div>
   );
